5681931661cafa20207f1a6498e967ed816e1627
NenukRC <126646428+NenukRC@users.noreply.github.com>
2023-05-01 12:34:42 +0000

    za

---
+++ test.js
@@ -0,0 +1,8 @@
+function Write(){
+    let jsname = document.getElementById("name").value;
+    let jsemail = document.getElementById("email").value;
+    let jsage = document.getElementById("age").value;
+    document.getElementById("1").innerHTML = jsname;
+    document.getElementById("2").innerHTML = jsemail;
+    document.getElementById("3").innerHTML = jsage;    
+}
---
+++ test3.js
@@ -0,0 +1,6 @@
+function Write(){
+    let jstorol = document.getElementById("torol").value;
+    let jsune = document.getElementById("une").value;
+    document.getElementById("1").innerHTML = jstorol;
+    document.getElementById("2").innerHTML = jsune;   
+}
